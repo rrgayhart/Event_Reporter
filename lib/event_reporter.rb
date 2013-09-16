c9f16b7bce9ff22395c8194308b95cd9e87b0c79
Meeka <rrgayhart@gmail.com>
2013-09-16 17:29:29 -0600

    Begins creating the run command and starts to split out run from other processess for testing reasons

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,11 +1,22 @@
 require 'csv'
 
 class EventReporter
-	def prompt(data)
-		data = gets.chomp
+	def initialize
 	end
+
+	def run
+		puts "Welcome to the Event Reporter"
+		prompt = ""
+			while prompt != "exit"
+				printf "enter command: "
+				input = gets.chomp.to_s
+				process_input(input)
+			end
+	end
+
+	#def prompt(data)
+	#	data = gets.chomp
+	#end
 end
-
-puts "Hello" 
-
-
+#eventreporter = EventReporter.new
+#eventreporter.run
